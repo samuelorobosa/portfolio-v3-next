6e395f0ab5e253263f07a78c1060b543ca76f4c3
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2023-09-27 15:52:29 +0100

    main: Updated projects

--- data/Projects/projectsData.jsx
+++ data/Projects/projectsData.jsx
@@ -1,22 +1,14 @@
 export const projectsData = [
     {
         id: 1,
-        title: "TaskScribe",
-        description: "A CLI Tool for Task Management",
-        techUsed: "Nodejs + Commanderjs",
+        title: "Roqqu",
+        description: "A cryptocurrency exchange platform.",
+        techUsed: "ReactJs + TypeScript",
         gitHubLink: null,
-        liveLink: "https://www.npmjs.com/package/taskscribe",
+        liveLink: "https://roqqu.com/",
     },
     {
         id: 2,
-        title: "Cordum",
-        description: "Cordum is a web application that enables user take notes in rich text and label them according to their respective categories.",
-        techUsed: "ReactJs + React Query, TypeScript",
-        gitHubLink: "https://github.com/samuelorobosa/cordum-frontend",
-        liveLink: "https://cordum.vercel.app/",
-    },
-    {
-        id: 3,
         title: "Niffy's Marketplace",
         description: "An NFT Landing Page.",
         techUsed: "ReactJs, TailwindCSS, SCSS, GSAP, Vercel",
@@ -24,7 +16,23 @@ export const projectsData = [
         liveLink: "https://niffys-marketplace.vercel.app/",
     },
     {
+        id: 3,
+        title: "TaskScribe",
+        description: "A CLI Tool for Task Management",
+        techUsed: "Nodejs + Commanderjs",
+        gitHubLink: null,
+        liveLink: "https://www.npmjs.com/package/taskscribe",
+    },
+    {
         id: 4,
+        title: "Cordum",
+        description: "Cordum is a web application that enables user take notes in rich text and label them according to their respective categories.",
+        techUsed: "ReactJs + React Query, TypeScript",
+        gitHubLink: "https://github.com/samuelorobosa/cordum-frontend",
+        liveLink: "https://cordum.vercel.app/",
+    },
+    {
+        id: 5,
         title: "SamuelOrobosa",
         description: "My Personal Website.",
         techUsed: "NextJS, TailwindCSS, Framer Motion",
@@ -32,34 +40,7 @@ export const projectsData = [
         liveLink: "https://samuelorobosa.vercel.app/",
     },
     {
-        id: 5,
-        title: "GitHub Profile Overview",
-        description: "GitHub Profile Search UI and Functionality.",
-        techUsed: "HTML5/CSS3/JavaScript, Bootstrap, GitHub API",
-        gitHubLink: "https://github.com/samuelorobosa/github-overview",
-        liveLink: "https://github-overview-ochre.vercel.app/",
-    },
-
-    {
         id: 6,
-        title: "Continent Search",
-        description: "Continents and Countries Search UI and Functionality.",
-        techUsed: "HTML5/CSS3/JavaScript, Bootstrap, Open Country API",
-        gitHubLink: "https://github.com/samuelorobosa/continent-search",
-        liveLink: "https://continent-search.netlify.app/",
-    },
-
-    {
-        id: 7,
-        title: "Orna's Form",
-        description: "Form UI template with error placeholders, validation, alerts and smooth animations.",
-        techUsed: "HTML5/CSS3/JavaScript, jQuery, Parsley.js",
-        gitHubLink: "https://github.com/samuelorobosa/SignUp_Login-Form",
-        liveLink: "https://signup-form-login.netlify.app/",
-    },
-
-    {
-        id: 8,
         title: "Fylo",
         description: "Landing page for a cloud storage service.",
         techUsed: "HTML5/CSS3, Bootstrap",
